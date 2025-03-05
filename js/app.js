4b21e99c74d24c397f6bc195bfba93897094986e
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-05 17:15:24 +0100

    feat: add imput and button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('ciaoooo')
+// chiedere all'utente i km
+const newKmElement = document.getElementById('new-km');
+// chiedere l'eta 
+const newAgeElement = document.getElementById('new-age');
+// richiamare il bottone 
+const buttElement = document.getElementById('butt');
+// richiamare il form
+const formElement = document.getElementById('my-form');
+// costo al km
+const priceKm = 0.21;
+
+
+console.log(newKmElement);
+console.log(buttElement);
+console.log(formElement);
+console.log(priceKm);
+
+
