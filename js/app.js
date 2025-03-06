b06d920ba6520c67d7b54e63e977df34da77ec54
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-06 10:50:12 +0100

    feat: mileston 2

--- js/app.js
+++ js/app.js
@@ -1,63 +1,64 @@
-// chiedere all'utente i km
-const newKmElement = document.getElementById('new-km');
-// chiedere l'eta 
-const newAgeElement = document.getElementById('new-age');
-// richiamare il bottone 
-const buttElement = document.getElementById('butt');
 // richiamare il form
 const formElement = document.getElementById('my-form');
-// costo al km
+// nome utetnte
+const newUserName = document.getElementById('new-user');
+const userInfoElement = document.getElementById('user-info')
+// chiedere all'utente i km
+const newKmElement = document.getElementById('new-km');
+const kmInfoElement = document.getElementById('km-info')
+// chiedere l'eta 
+const newAgeElement = document.getElementById('new-age');
+const ageInfoElement = document.getElementById('age-info')
+// prezzo km
 const priceKm = 0.21;
-
-
-console.log(newKmElement);
-console.log(buttElement);
-console.log(formElement);
-console.log(priceKm);
-
+const priceInfoElement = document.getElementById('price-info')
 
 formElement.addEventListener('submit', function (event) {
 
   event.preventDefault();
-const userKm = parseInt(newKmElement.value);
-console.log(userKm);
-const userAge = parseInt(newAgeElement.value);
-console.log(userAge);
-//calcoli dello sconto e del prezzo
-  //se i dati sono validi
-  const isDataValid = !isNaN(userKm) && !isNaN(userAge);
-  //se i dati sono maggiori di zero
-  const isDataBiggerThanZero = userAge > 0 && userKm > 0;
-  //se età compresa tra 1-99
-  const isAgeValid = userAge > 1 && userAge < 99;
+  // volore nome 
+  const userName = newUserName.value;
+  console.log(userName)
+  // valore dei km
+  const userKm = parseInt(newKmElement.value);
+  console.log(userKm);
+  // valore anni utente
+  const userAge = parseInt(newAgeElement.value);
+  console.log(userAge);
+
+ //calcolo del prezzo base
+  let basePrice = priceKm * userKm;
+  //calcolo della percentuale
+  let discountPercentage = 0;
+  
+  if (userAge < 18) {
+    discountPercentage = 20;
+    console.log('sconto minorenne');
+  } else if (userAge >= 65) {
+    discountPercentage = 40;
+    console.log('sconnto over')
+  }
+  
+  let discount = (basePrice * discountPercentage) / 100;
+  //valore dello sconto da sottrarre al prezzo finale
+  basePrice = basePrice - discount;
+  //calcolo del prezzo formattato in euro
+  let formattedPrice = new Intl.NumberFormat('it-IT', {
+    style: 'currency',
+    currency: 'EUR'
+  }).format(basePrice);
+  
+  //stampa del prezzo
+  console.log(`Il prezzo finale del biglietto è ${formattedPrice}`);
   
   
-  const validCondition = isDataValid && isDataBiggerThanZero && isAgeValid;
-  
-  if (validCondition) {
-  
-    //calcolo del prezzo base
-    let basePrice = priceKm * userKm;
-    //calcolo della percentuale
-    let discountPercentage = 0;
-  
-    if (userAge < 18) {
-      discountPercentage = 20;
-    } else if (userAge >= 65) {
-      discountPercentage = 40;
-    }
-  
-    let discount = (basePrice * discountPercentage) / 100;
-    //valore dello sconto da sottrarre al prezzo finale
-    basePrice = basePrice - discount;
-    //calcolo del prezzo formattato in euro
-    let formattedPrice = new Intl.NumberFormat('it-IT', {
-      style: 'currency',
-      currency: 'EUR'
-    }).format(basePrice);
-  
-    //stampa del prezzo
-    console.log(`Il prezzo finale del biglietto è ${formattedPrice}`);
-  } else {
-      console.error('I dati inseriti non sono corretti');
-    }})
+  // stampa nome nel biglietto
+  userInfoElement.innerText = userName;
+  kmInfoElement.innerText = userKm;
+  ageInfoElement.textContent = userAge;
+  priceInfoElement.innerText = formattedPrice;
+
+})
+
+
+
